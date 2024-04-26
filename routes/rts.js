ca715a6c9a4a75b2018f040207e879542d269195
Justina Ominisan <justinaominisan@gmail.com>
2024-04-26 09:10:56 +0100

    initial

--- routes/rts.js
+++ routes/rts.js
@@ -7,7 +7,7 @@ const {contact} = require("../controllers/contact")
 const {home} = require("../controllers/ctrl");
 
 // Home page
-routerManager.get("/api/v1", home);
+routerManager.get("/", home);
 
 
 //Vendor routes
@@ -18,15 +18,25 @@ routerManager.put("/restaurant:id", restaurant);
 
 // Menu and search
 routerManager.put("/menu/:id", menu);
+routerManager.get("/menu/:id", menu);
+routerManager.post("/menu/:id", menu);
 
 
 // Contact
 routerManager.put("/contact", contact);
+routerManager.post("/contact", contact);
+routerManager.get("/contact", contact);
 
 // User authentication
 
 routerManager.post("/auth/register", register)
+
+routerManager.put("/auth/register", register)
+
+routerManager.get("/auth/register", register)
 routerManager.post("/auth/login", login);
+routerManager.put("/auth/login", login);
+routerManager.get("/auth/login", login);
 
 
 // Error handling
